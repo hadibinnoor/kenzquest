94af5f8fa4944e2b959f8601685c82f84f847336
Rango <rango@localhost.localdomain>
2023-02-16 00:21:28 +0530

    hopeisfinal

--- routes/treasureRoute.js
+++ routes/treasureRoute.js
@@ -8,11 +8,47 @@ router.post('/', (req, res) => {
     let teamcode = req.body.teamcode;
     let word = req.body.word;
     const newWord = word.toLowerCase();
-    if (teamcode == 2744 && newWord == "terminator") {
+    if (teamcode == 4005 && newWord == "bandwidth") {
         res.render('firstpage')
-    } else if (teamcode == 2642 && newWord == "transformer") {
+    } else if (teamcode == 7647 && newWord == "bandwidth") {
         res.render('secondpage')
-    } else if (teamcode == 8462 && newWord == "transformer") {
+    } else if (teamcode == 2580 && newWord == "bandwidth") {
+        res.render('thirdpage')
+    } else if (teamcode == 6630 && newWord == "bandwidth") {
+        res.render('firstpage')
+    } else if (teamcode == 5462 && newWord == "bandwidth") {
+        res.render('firstpage')
+    } else if (teamcode == 2808 && newWord == "bandwidth") {
+        res.render('secondpage')
+    } else if (teamcode == 0374 && newWord == "bandwidth") {
+        res.render('secondpage')
+    } else if (teamcode == 4186 && newWord == "bandwidth") {
+        res.render('thirdpage')
+    } else if (teamcode == 9754 && newWord == "bandwidth") {
+        res.render('thirdpage')
+    } else if (teamcode == 6876 && newWord == "bandwidth") {
+        res.render('firstpage')
+    } else if (teamcode == 1298 && newWord == "bandwidth") {
+        res.render('firstpage')
+    } else if (teamcode == 3424 && newWord == "bandwidth") {
+        res.render('secondpage')
+    } else if (teamcode == 7869 && newWord == "bandwidth") {
+        res.render('secondpage')
+    } else if (teamcode == 9210 && newWord == "bandwidth") {
+        res.render('thirdpage')
+    } else if (teamcode == 7267 && newWord == "bandwidth") {
+        res.render('thirdpage')
+    } else if (teamcode == 5394 && newWord == "bandwidth") {
+        res.render('firstpage')
+    } else if (teamcode == 0862 && newWord == "bandwidth") {
+        res.render('secondpage')
+    } else if (teamcode == 1435 && newWord == "bandwidth") {
+        res.render('thirdpage')
+    } else if (teamcode == 6135 && newWord == "bandwidth") {
+        res.render('firstpage')
+    } else if (teamcode == 8276 && newWord == "bandwidth") {
+        res.render('secondpage')
+    } else if (teamcode == 5801 && newWord == "bandwidth") {
         res.render('thirdpage')
     }
     else {
