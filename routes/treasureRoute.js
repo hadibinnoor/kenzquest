55f07490996ac9673e8b6481dcb42a3c2c624314
Rango <rango@localhost.localdomain>
2023-02-15 11:43:08 +0530

    test

--- routes/treasureRoute.js
+++ routes/treasureRoute.js
@@ -1,8 +1,22 @@
 const express = require('express')
-const treasureController = require('../controllers/treasureController')
 const router = express.Router()
 
-router.get('/', treasureController.getHomePage)
-router.post('/', treasureController.postHomePage)
-
+router.get('/', (req,res) => {
+    res.render('home')
+})
+router.post('/', (req, res) => {
+    let teamcode = req.body.teamcode;
+    let word = req.body.word;
+    const newWord = word.toLowerCase();
+    if (teamcode == 2744 && newWord == "terminator") {
+        res.render('firstpage')
+    } else if (teamcode == 2642 && newWord == "transformer") {
+        res.render('two')
+    } else {
+        res.render('404')
+    }
+})
+router.get('/firstpage', (req,res) => {
+    res.render('firstpage')
+});
 module.exports = router;
